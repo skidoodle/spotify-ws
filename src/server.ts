0c760fa3475f73b16efd6bf41698fc44df54d51f
skidoodle <contact@albert.lol>
2024-01-10 09:42:42 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -1,72 +1,72 @@
-import * as http from 'http';
-import { Server as SocketIOServer, Socket } from 'socket.io';
-import 'dotenv/config';
+import http from 'http'
+import { Server as SocketIOServer, Socket } from 'socket.io'
+import 'dotenv/config'
 
 // Mostly used for local development
-const HOST = process.env.HOST || 'http://localhost';
-const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'http://localhost'
+const PORT = process.env.PORT || 3000
 
 // Spotify nowplaying api endpoint (e.g https://albert.lol/api/spotify)
-const ENDPOINT = process.env.ENDPOINT;
+const ENDPOINT = process.env.ENDPOINT
 
-const server = http.createServer();
+const server = http.createServer()
 const io = new SocketIOServer(server, {
   cors: {
     origin: '*',
   },
-});
+})
 
-let previousData: any = null;
+let previousData: any = null
 
 const sendNowPlayingData = async () => {
   try {
-    const response = await fetch(`${ENDPOINT}`);
-    const data = await response.json();
+    const response = await fetch(`${ENDPOINT}`)
+    const data = await response.json()
 
     if (data) {
       if (!isEqual(data, previousData)) {
-        io.emit('nowPlayingData', data);
-        previousData = data;
+        io.emit('nowPlayingData', data)
+        previousData = data
       }
     } else {
-      console.error('Invalid data');
+      console.error('Invalid data')
     }
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching data:', error)
   }
-};
-
-function isEqual(obj1: any, obj2: any) {
-  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
-server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
-  if (req.url === '/') {
-    res.writeHead(426, { 'Content-Type': 'text/plain' });
-    res.end('Upgrade Required');
-  } else {
-    return;
-  }
-});
+function isEqual(obj1: any, obj2: any) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2)
+}
+
+// server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
+//   if (req.url === '/') {
+//     res.writeHead(426, { 'Content-Type': 'text/plain' })
+//     res.end('Upgrade Required')
+//   } else {
+//     return
+//   }
+// })
 
 io.on('connection', (socket: Socket) => {
   if (previousData) {
-    socket.emit('nowPlayingData', previousData);
+    socket.emit('nowPlayingData', previousData)
   }
 
   const intervalId = setInterval(() => {
-    sendNowPlayingData();
-  }, 3000);
+    sendNowPlayingData()
+  }, 3000)
 
   socket.on('disconnect', () => {
-    clearInterval(intervalId);
-  });
+    clearInterval(intervalId)
+  })
 
   socket.on('error', (err) => {
-    console.error('WebSocket error:', err);
-  });
-});
+    console.error('WebSocket error:', err)
+  })
+})
 
 server.listen(PORT, () => {
-  console.log(`[SPOTIFY-WS] STARTED - ${HOST}:${PORT}`);
-});
+  console.log(`[SPOTIFY-WS] STARTED - ${HOST}:${PORT}`)
+})
