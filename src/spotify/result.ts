ef18b423d8dee43dfef212f559ff2d6a4c211a17
skidoodle <contact@albert.lol>
2024-03-12 19:17:21 +0100

    Add SongResultMap class to parse song results

---
+++ src/spotify/result.ts
@@ -0,0 +1,28 @@
+import type { SongResult, Artist, Item } from './types'
+
+export class SongResultMap {
+  public static parseSong(result: {
+    progress_ms: number
+    item: Item
+    is_playing: boolean
+  }): SongResult {
+    const { item } = result
+
+    return {
+      is_playing: result.is_playing,
+      title: item.name,
+      album: {
+        name: item.album.name,
+        image: item.album.images[0]?.url,
+        release_date: item.album.release_date,
+      },
+      artists: {
+        name: item.artists.map((x: Artist) => x.name),
+        url: item.artists.map((x: Artist) => x.external_urls.spotify),
+      },
+      url: item.external_urls.spotify,
+      progress: result.progress_ms,
+      duration: item.duration_ms,
+    }
+  }
+}
