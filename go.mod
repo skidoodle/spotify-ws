c9806d5fe9c79ffafb71150af119cb74742bc7dc
skidoodle <contact@albert.lol>
2025-02-28 21:46:05 +0000

    docker healthcheck

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/zmb3/spotify v1.3.0
-	golang.org/x/oauth2 v0.24.0
+	golang.org/x/oauth2 v0.27.0
 )
 
-require golang.org/x/sys v0.28.0 // indirect
+require golang.org/x/sys v0.30.0 // indirect
--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	resp, err := http.Get("http://localhost:3000/health")
-	if err != nil || resp.StatusCode != 200 {
+	if err != nil {
+		log.Fatalf("Error performing health check: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Health check failed: Status code %d", resp.StatusCode)
 		os.Exit(1)
 	}
+
+	fmt.Println("OK")
 	os.Exit(0)
 }
